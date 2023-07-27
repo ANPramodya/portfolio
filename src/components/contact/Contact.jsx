f3f0a01fa9ac54aaa8780c85be876f1e3a875b91
ANPramodya <86116471+ANPramodya@users.noreply.github.com>
2023-07-27 13:32:07 +0530

    contact options

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,57 @@
 import React from "react";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
 import "./contact.css";
 
 const Contact = () => {
   return (
     <section id="contact">
-      <div>Contact</div>
+      <h5>-Contact</h5>
+      <div className="contact-container">
+        <div className="contact-options">
+          <h2>
+            Got a project?
+            <br /> Let's talk.
+          </h2>
+          <article className="contact-option">
+            <MdOutlineEmail />
+            <h4>Email</h4>
+            <h5>anpramodya@gmail.com</h5>
+            <a href="mailto:anpramodya@gmail.com" target="_blank">
+              Send a Message
+            </a>
+          </article>
+          <article className="contact-option">
+            <RiMessengerLine />
+            <h4>Messenger</h4>
+            <h5>Nipun Pramodya</h5>
+            <a href="" target="_blank">
+              Send a Message
+            </a>
+          </article>
+          <article className="contact-option">
+            <BsWhatsapp />
+            <h4>WhatsApp</h4>
+            <h5>+94767065262</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=+94767065262"
+              target="_blank"
+            >
+              Send a Message
+            </a>
+          </article>
+        </div>
+        <div className="contact-form">
+          <form action="" className="contact-formInputs">
+            <input type="text" placeholder="Your Name" />
+            <input type="text" placeholder="Your Email" />
+            <input type="text" placeholder="Your Content" />
+            <button>Send</button>
+          </form>
+        </div>
+      </div>
+      <div className="contact-ending">This is the ending</div>
     </section>
   );
 };
